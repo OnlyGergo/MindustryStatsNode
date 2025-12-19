68544660b2fe852199b75003ad5035a1cd5d751f
Gergo EB <your-email@example.com>
2025-12-19 17:41:34 +0000

    Bump version Update fonts

--- common/version.ts
+++ common/version.ts
@@ -1,3 +1,3 @@
 export const VERSION = 'v3-staging';
-export const COMMIT = '6284703';
-export const BUILD_DATE = 'Fri 19 Dec 17:23:36 GMT 2025';
+export const COMMIT = '40d29ae';
+export const BUILD_DATE = 'Fri 19 Dec 17:41:13 GMT 2025';
